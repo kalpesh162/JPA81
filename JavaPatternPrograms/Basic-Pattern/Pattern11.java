72ac0c3d1d03b0fda272b775f0821360a8d13c99
kalpesh <kalpesh162@gmail.com>
2025-08-04 12:49:41 +0530

    Pattern11 Added

---
+++ JavaPatternPrograms/Basic-Pattern/Pattern11.java
@@ -0,0 +1,13 @@
+/*
+
+*/
+class Pattern11{
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=n;i>=1;i--){
+			for(int j=1;j<=n;j++)
+				System.out.print((char)(64+i));
+			System.out.println();
+		}
+	}
+}
