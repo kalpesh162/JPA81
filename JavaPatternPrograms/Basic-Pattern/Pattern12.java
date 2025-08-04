72ee9fbc1c43e7398f66fd484bf230ca9e1d6fc1
kalpesh <kalpesh162@gmail.com>
2025-08-04 12:51:37 +0530

    Pattern 12 Added

---
+++ JavaPatternPrograms/Basic-Pattern/Pattern12.java
@@ -0,0 +1,13 @@
+/*
+
+*/
+class Pattern12{
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=n;i>=1;i--){
+			for(int j=1;j<=n;j++)
+				System.out.print((char)(96+i));
+			System.out.println();
+		}
+	}
+}
